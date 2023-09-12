418b02c5535410256fe828f5193eab9078f39e2b
likahey99 <likahey@outlook.com>
2023-09-12 23:37:57 +0100

    restored populate gucc cos i deleted it rather than gear store

--- populate_guccSite.py
+++ populate_guccSite.py
@@ -9,7 +9,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'groupProject.settings')
 
 django.setup()
-from gearStore.models import Gear, Category, PageContents
+from guccSite.models import Gear, Category, PageContents
 
 
 def populate():
@@ -56,27 +56,15 @@ def populate():
          'logo_image': 'site_images/default_logo.png',
          'home_contents': 'Welcome to Gear Store! Gear Store is a website where you can find and borrow gear for sports! If you want to browse the gear available to be borrowed, take a look at the options in the left-hand sidebar, or browse the options in the Find Gear tab.',
          'about_contents': '''Hi, welcome to Gear Store! Here, you are able to view various pieces of gear available to be borrowed, and add them to your own account.
-
-
-If you don't already have an account, feel free to create one and log in, and view the My Account page. Here you can manage your account and your bookings, as well as logout.
-
-
-If you are a manager of the club, please create an account and enter your admin password to become a site admin. Then, you will be able to add gear and categories, as well as view all current bookings.
+        If you don't already have an account, feel free to create one and log in, and view the My Account page. Here you can manage your account and your bookings, as well as logout.
+        If you are a manager of the club, please create an account and enter your admin password to become a site admin. Then, you will be able to add gear and categories, as well as view all current bookings.
          ''',
          'contact_contents': '''Here is how you can contact us if you have any questions:
-
-
-If you want to ask us any questions regarding using this website, please email:
-
-help@gearstore.com
-
-
-If you have any questions about borrowing gear, please email:
-
-borrow@gearstore.com
-
-
-You can drop us a follow on instagram @gear-store
+        If you want to ask us any questions regarding using this website, please email:
+        help@guccsite.com
+        If you have any questions about borrowing gear, please email:
+        borrow@gucchelp.com
+        You can drop us a follow on instagram @gusaCanoe
          ''',
          'contact': '07933 123 456',
          'contact_option': 'logo-whatsapp',
